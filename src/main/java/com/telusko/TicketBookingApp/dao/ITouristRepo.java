45e2977fa2b8125e193abe9eae17d331770b72ef
chandrareddymoola <cmr.202116@gmail.com>
2024-07-26 23:40:12 +0530

    first commit

---
+++ src/main/java/com/telusko/TicketBookingApp/dao/ITouristRepo.java
@@ -0,0 +1,12 @@
+package com.telusko.TicketBookingApp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.telusko.TicketBookingApp.Model.Tourist;
+
+@Repository 
+public interface ITouristRepo extends JpaRepository<Tourist, Integer>
+{
+
+}
